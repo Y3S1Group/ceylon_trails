5a80090060a77f46fd80101c7c7e79680f84d563
ravindu422 <thiranjayaravindu@gmail.com>
2025-08-18 23:23:25 +0530

    Initialize server connection

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+
+//dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5006;
+
+//connectDB();
+
+app.use(express.json());
+
+app.use (cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+}));
+
+app.use((req, res, next) => {
+    console.log('Request body before route:',req.body);
+    next();
+});
+
+//api routes
+app.get('/', (req, res) => {
+    res.send('Hello, from Server')
+})
+
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
