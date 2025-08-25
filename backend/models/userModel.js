be452a2b9e146b3cd5dbf2c99bcfb84d178d26cb
hiruvindajayashanka2001 <hiruvindajayashanka2001@gmail.com>
2025-08-25 16:24:37 +0530

    Create user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    verifyOtp: {type: String, default: ''},
+    verifyOtpExpires: {type: Number, default: 0},
+    isVerified: {type: Boolean, default: false},
+    resetOtp: {type: String, default: ''},
+    resetOtpExpires: {type: Number, default: 0},
+})
+
+const userModel = mongoose.model.user || mongoose.model('User', userSchema);
+export default userModel;
+
+
