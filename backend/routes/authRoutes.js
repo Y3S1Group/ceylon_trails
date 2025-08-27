54c5cd71f346cb2c502d3b5a40e4fb4707565a5c
hiruvindajayashanka2001 <hiruvindajayashanka2001@gmail.com>
2025-08-27 13:30:07 +0530

    create endpoint for getcurrentuser

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, login, logout, sendVerifyOtp, verifyEmail} from '../controllers/authController.js';
+import { register, login, logout, sendVerifyOtp, verifyEmail, getCurrentUser} from '../controllers/authController.js';
 import userAuth from '../middleware/userauth.js';
 
 const authRouter = express.Router();
@@ -9,6 +9,6 @@ authRouter.post('/login', login);
 authRouter.post('/logout', logout);
 authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp);
 authRouter.post('/verify-account', userAuth, verifyEmail);
-
+authRouter.post('/get-current-user', userAuth, getCurrentUser);
 
 export default authRouter;
